9c63cc45249d21f5ae835bd2ff18e7c505f4ae61
prainsa <hamuod@gmail.com>
2025-01-11 00:21:35 +0300

    second

---
+++ trip.py
@@ -0,0 +1,10 @@
+def swap(a, b,):
+    print("Before swapping: a =", a, ", b =", b)
+
+    a, b = b, a
+    
+    print("After swapping: a =", a, ", b =", b)
+
+num1 = 10
+num2 = 20
+swap(num1, num2)
